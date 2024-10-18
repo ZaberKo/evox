e5d67b7115af87f78492b718aab93be09d6cd976
zaber <20830726+ZaberKo@users.noreply.github.com>
2024-10-18 16:11:46 +0800

    refine ARS

--- src/evox/algorithms/so/es_variants/ars.py
+++ src/evox/algorithms/so/es_variants/ars.py
@@ -12,71 +12,75 @@
 import jax
 import jax.numpy as jnp
 import optax
-import evox
+
+from evox import Algorithm, State, use_state, utils
 
 
-@evox.jit_class
-class ARS(evox.Algorithm):
+class ARS(Algorithm):
     def __init__(
         self,
-        pop_size: int,
         center_init: jax.Array,
-        elite_ratio: float = 0.1,
+        pop_size: int,
+        num_elites: int,
         optimizer: str = "adam",
         lr: float = 0.05,
         sigma: float = 0.03,
+        eps: float = 1e-08,
     ):
         super().__init__()
 
-        assert not pop_size & 1
-        assert 0 <= elite_ratio <= 1
+        assert (
+            pop_size > 0 and pop_size % 2 == 0
+        ), "pop_size must be a positive even number"
 
         self.dim = center_init.shape[0]
         self.center_init = center_init
         self.pop_size = pop_size
+        self.num_elites = num_elites
         self.lr = lr
         self.sigma = sigma
-
-        self.elite_ratio = elite_ratio
-        self.elite_pop_size = max(1, int(self.pop_size / 2 * self.elite_ratio))
+        self.eps = eps
 
         if optimizer == "adam":
-            self.optimizer = optax.adam(learning_rate=lr)
+            self.optimizer = utils.OptaxWrapper(
+                optax.adam(learning_rate=lr), center_init
+            )
         else:
-            raise NotImplementedError
-
-        self.optimizer = evox.utils.OptaxWrapper(self.optimizer, center_init)
+            self.optimizer = None
 
     def setup(self, key):
-        return evox.State(
+        return State(
             key=key,
             center=self.center_init,
+            delta=jax.random.normal(key, (self.pop_size // 2, self.dim)),
         )
 
     def ask(self, state):
-        key, _ = jax.random.split(state.key)
-        z_plus = jax.random.normal(
-            state.key,
-            (int(self.pop_size / 2), self.dim),
-        )
+        key, sample_key = jax.random.split(state.key)
+        z_plus = jax.random.normal(sample_key, (self.pop_size // 2, self.dim))
         z = jnp.concatenate([z_plus, -1.0 * z_plus])
         x = state.center + self.sigma * z
-        return x, state.replace(key=key, population=x, noise=z)
+        return x, state.replace(key=key, delta=z_plus)
 
     def tell(self, state, fitness):
-        noise_1 = state.noise[: int(self.pop_size / 2)]
-        fit_1 = fitness[: int(self.pop_size / 2)]
-        fit_2 = fitness[int(self.pop_size / 2) :]
-        elite_idx = jnp.minimum(fit_1, fit_2).argsort()[: self.elite_pop_size]
+        half_pop_size = self.pop_size // 2
+
+        fit_1 = fitness[:half_pop_size]
+        fit_2 = fitness[half_pop_size:]
+        elite_idx = jax.lax.top_k(-jnp.minimum(fit_1, fit_2), self.num_elites)[1]
 
         fitness_elite = jnp.concatenate([fit_1[elite_idx], fit_2[elite_idx]])
         # Add small constant to ensure non-zero division stability
-        sigma_fitness = jnp.std(fitness_elite) + 1e-05
-        fit_diff = fit_1[elite_idx] - fit_2[elite_idx]
-        fit_diff_noise = jnp.dot(noise_1[elite_idx].T, fit_diff)
-        theta_grad = 1.0 / (self.elite_pop_size * sigma_fitness) * fit_diff_noise
+        fitness_std = jnp.std(fitness_elite) + self.eps
 
-        updates, state = evox.use_state(self.optimizer.update)(state, theta_grad, state.center)
-        center = optax.apply_updates(state.center, updates)
+        fit_diff = fit_1[elite_idx] - fit_2[elite_idx]
+
+        grad = (state.delta[elite_idx] @ fit_diff) / (self.num_elites * fitness_std)
+
+        if self.optimizer is None:
+            center = state.center - self.lr * grad
+        else:
+            updates, state = use_state(self.optimizer.update)(state, grad)
+            center = optax.apply_updates(state.center, updates)
 
         return state.replace(center=center)
